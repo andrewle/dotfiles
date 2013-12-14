0d5e563fad3f7dfc4ae44e1a559ccff82a70ab30
Andrew Le <andrew@andrewdle.com>
2013-12-14 14:55:27 -0800

    Set up env in fish and supress default greeting

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,15 @@
+# Environment #############################
+set fish_greeting ""
+set -xg JAVA_HOME /System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home
+set -xg GOBIN "~/Projects/golang/bin"
+set -xg NPM_BIN /usr/local/share/npm/bin
+set -xg POSTGRESAPP /Applications/Postgres.app/Contents/MacOS/bin
+set -xg PATH ~/bin $POSTGRESAPP $PATH $NPM_BIN
+set -xg CLICOLOR true
+set -xg LSCOLORS "exfxcxdxbxegedabagacad"
+set -xg PAGER less
+set -xg LESS "-RSXF --tabs=4"
+
 # Command Aliases ##########################
 alias photoshop="open -a /Applications/Adobe\ Creative\ Suite\ 3/Adobe\ Photoshop\ CS3/Adobe\ Photoshop\ CS3.app/"
 alias preview="open -a preview"
