8828f2fa058e256cd5e55cb1aae5bde43934e1a3
Andrew Le <andrew@andrewdle.com>
2014-09-09 11:15:52 -0700

    Fix rvm fish integration

--- config/fish/functions/rvm.fish
+++ config/fish/functions/rvm.fish
@@ -4,7 +4,8 @@ function rvm --description='Ruby enVironment Manager'
   bash -c 'source ~/.rvm/scripts/rvm; rvm "$@"; status=$?; env > "$0"; exit $status' $env_file $argv
 
   # apply rvm_* and *PATH variables from the captured environment
-  and eval (grep '^rvm\|^[^=]*PATH\|^GEM_HOME' $env_file | grep -v '_clr=' | sed '/^[^=]*PATH/y/:/ /; s/^/set -xg /; s/=/ /; s/$/ ;/; s/(//; s/)//')
+  # and eval (grep '^rvm\|^[^=]*PATH\|^GEM_HOME' $env_file | grep -v '_clr=' |)
   # clean up
+  eval (grep '^rvm\|^[^=]*PATH\|^GEM_HOME\|^MY_RUBY_HOME' $env_file | grep -v '_clr=' | grep -v 'rvm_debug' | sed '/^[^=]*PATH/y/:/ /; s/^/set -xg /; s/=/ /; s/$/ ;/; s/(//; s/)//')
   rm -f $env_file
 end
