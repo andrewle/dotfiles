bb7aa2c4ce293e5bb7c302e60ca70b803238e63d
Andrew Le <andrew@andrewdle.com>
2016-06-29 16:09:36 -0700

    Simply gitroot helper

--- git/helpers.sh
+++ git/helpers.sh
@@ -1,36 +1,13 @@
 #!/bin/bash
 
-function gitroot {
+gitroot() {
   if [ "$(pwd)" != "$(git rev-parse --show-toplevel)" ]; then
-    go-to-git-root
+    go_to_git_root
   elif [ $(cd .. && git rev-parse --is-inside-work-tree &> /dev/null; echo $?) -eq 0 ]; then
-    cd .. && go-to-git-root
+    cd .. && go_to_git_root
   fi
 }
 
-function go-to-git-root {
+go_to_git_root() {
   cd $(git rev-parse --show-toplevel)
 }
-
-function git-last-branch {
-  current_branch=$(git branch | grep -v '(no branch)' | grep "\*" |
-  awk '{print $2}')
-
-  # support when running this from a detached head
-  if [ "${current_branch}" == "" ]; then
-    current_branch=$(git rev-list --max-count=1 HEAD)
-  fi;
-
-  git reflog | \
-    grep -m 1 "checkout:.*to ${current_branch}$" | \
-    sed 's/.*from \([^ ]*\).*/\1/'
-}
-
-function git-checkout-last-branch {
-  git checkout $(git-last-branch)
-}
-
-function git-merge-last-branch {
-  git merge --no-ff $(git-last-branch)
-}
-
